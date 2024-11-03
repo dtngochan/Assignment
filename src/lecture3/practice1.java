2ac7e7413bb8cc0a152d2945bc2a97b69faa91fa
dtngochan <dtngochan0604@gmail.com>
2024-11-03 16:36:32 +0700

    Assignment 2

--- src/lecture3/practice1.java
+++ src/lecture3/practice1.java
@@ -1,30 +1,30 @@
-
 package lecture3;
 
 import java.util.Scanner;
 
 public class practice1 {
-    public static void main(String[] args){
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter your name: ");
-    String name = scanner.nextLine();
-    System.out.println("Enter your age: ");
-    int age = scanner.nextInt();
-    System.out.println("Enter your sex: ");
-    String sex = scanner.next(); 
-    scanner.nextLine();
-    System.out.println("Enter your major: ");
-    String major = scanner.nextLine();
-    System.out.println("Enter your GPA: ");
-    float GPA = scanner.nextFloat();
-    scanner.nextLine();
-    System.out.println("Enter your hometown: ");
-    String hometown = scanner.nextLine();
-    System.out.println("Name: " +name);
-    System.out.println("Age: " +age);
-    System.out.println("Set: " +sex);
-    System.out.println("Major: " +major);
-    System.out.println("GPA: " +GPA);
-    System.out.println("Hometown: " +hometown);
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+        System.out.println("Enter your sex: ");
+        String sex = scanner.next();
+        scanner.nextLine();
+        System.out.println("Enter your major: ");
+        String major = scanner.nextLine();
+        System.out.println("Enter your GPA: ");
+        float GPA = scanner.nextFloat();
+        scanner.nextLine();
+        System.out.println("Enter your hometown: ");
+        String hometown = scanner.nextLine();
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Sex: " + sex);
+        System.out.println("Major: " + major);
+        System.out.println("GPA: " + GPA);
+        System.out.println("Hometown: " + hometown);
     }
 }
